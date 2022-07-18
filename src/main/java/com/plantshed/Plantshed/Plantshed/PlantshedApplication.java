6ce6849ffa83d400487d549d6c339be09b158d89
Eli-ssaD <notjusthandbags@hotmail.co.uk>
2022-07-18 17:22:16 +0100

    Bootstrap Project

---
+++ src/main/java/com/plantshed/Plantshed/Plantshed/PlantshedApplication.java
@@ -0,0 +1,13 @@
+package com.plantshed.Plantshed.Plantshed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantshedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantshedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plantshed/Plantshed/Plantshed/PlantshedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantshed.Plantshed.Plantshed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantshedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
